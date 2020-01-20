8d66326cedccd93b96df0b2f80a867d0bf372cf8
John Jullies Palma <johnjullies@gmail.com>
2020-01-20 14:46:05 +0800

    Use range to simplify loop statement

--- Exercises/exercise-slices.go
+++ Exercises/exercise-slices.go
@@ -7,8 +7,8 @@ func Pic(dx, dy int) [][]uint8 {
   for i := range pic {
     pic[i] = make([]uint8, dx)
   }
-  for y := 0; y < dy; y++ {
-    for x := 0; x < dx; x++ {
+  for y := range pic {
+    for x := range pic[y] {
       // pic[x][y] = uint8(  )
       // pic[x][y] = uint8( (x+y)/2 )
       // pic[x][y] = uint8( x*x+y*y )
