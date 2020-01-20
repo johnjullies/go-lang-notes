74ce6330880fb246927324af6f8d3678957e33ba
John Jullies Palma <johnjullies@gmail.com>
2020-01-20 14:45:22 +0800

    Refactor and simplify the solution
    Remove check helper function which is apparently not
    necessary. The initial value of temp can just be z then after
    every iteration of the loop, temp gets the new float64 value
    of z. This makes the code cleaner and simpler.

--- Exercises/loops-and-functions.go
+++ Exercises/loops-and-functions.go
@@ -6,20 +6,17 @@ import (
 
 func Sqrt(x float64) float64 {
   z := 1.0
-  temp := fmt.Sprintf("%.4f", z)
+  temp := z
   for i := range make([]int, 10) {
     z -= (z*z - x) / (2*z)
-    if check(z, temp) {
+    fmt.Printf("%d\t%.4f\n", i, z)
+    check := fmt.Sprintf("%.4f", z) == fmt.Sprintf("%.4f", temp)
+    if check {
       return z
     }
-    temp = fmt.Sprintf("%.4f", z)
-    fmt.Printf("%d\t%.4f\n", i, z)
+    temp = z
   }
   return z
-}
-
-func check(z float64, temp string) bool {
-  return fmt.Sprintf("%.4f", z) == temp
 }
 
 func main() {
